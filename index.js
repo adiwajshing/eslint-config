14c06285befa0952ccdc9e3f8e28aed87f42aa72
ammarSura <ammarsura@gmail.com>
2025-09-15 21:03:32 +0530

    fix: use flat config

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { defineConfig } from "eslint/config";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import react from "eslint-plugin-react";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
@@ -6,239 +5,225 @@ import unicorn from "eslint-plugin-unicorn";
 import jest from "eslint-plugin-jest";
 import globals from "globals";
 import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
 import stylistic from '@stylistic/eslint-plugin'
 
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default defineConfig([{
-    files: ["src/**/*.{js,ts,tsx}"],
-    extends: compat.extends("plugin:react/recommended"),
-
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-        react,
-        "simple-import-sort": simpleImportSort,
-        unicorn,
-        jest,
-        '@stylistic': stylistic,
-    },
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...jest.environments.globals.globals,
+export default [
+    react.configs.flat.recommended,
+    {
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+            react,
+            "simple-import-sort": simpleImportSort,
+            unicorn,
+            jest,
+            '@stylistic': stylistic,
         },
 
-        parser: tsParser,
-        ecmaVersion: 5,
-        sourceType: "module",
-        parserOptions: {
-          projectService: true,
-          tsconfigRootDir: import.meta.dirname,
-        }
-    },
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...jest.environments.globals.globals,
+            },
 
-    settings: {
-        react: {
-            version: "detect",
+            parser: tsParser,
+            ecmaVersion: 5,
+            sourceType: "module",
+            parserOptions: {
+            projectService: true,
+            tsconfigRootDir: import.meta.dirname,
+            }
         },
-    },
 
-    rules: {
-        "unicorn/no-array-for-each": ["error"],
-
-        "unicorn/consistent-function-scoping": ["error", {
-            checkArrowFunctions: false,
-        }],
-
-        "unicorn/no-await-expression-member": "error",
-        "unicorn/no-for-loop": "error",
-        "unicorn/no-lonely-if": "error",
-        "unicorn/no-object-as-default-parameter": "error",
-        "unicorn/prefer-array-find": "error",
-        "unicorn/prefer-array-some": "error",
-        "max-depth": ["error", 4],
-
-        "@typescript-eslint/no-misused-promises": ["error", {
-            checksSpreads: true,
-            checksVoidReturn: false,
-            checksConditionals: true,
-        }],
-
-        "@typescript-eslint/prefer-optional-chain": ["error"],
-        "@typescript-eslint/no-unnecessary-type-assertion": ["error"],
-        "@typescript-eslint/no-unnecessary-type-constraint": ["error"],
-        "@typescript-eslint/no-redundant-type-constituents": ["error"],
-        "@typescript-eslint/no-inferrable-types": ["error"],
-
-        "@typescript-eslint/no-explicit-any": ["warn", {
-            ignoreRestArgs: true,
-        }],
-
-        "@stylistic/type-annotation-spacing": ["error"],
-
-        "@stylistic/member-delimiter-style": ["error", {
-            multiline: {
-                delimiter: "none",
-                requireLast: false,
+        settings: {
+            react: {
+                version: "detect",
             },
+        },
 
-            singleline: {
-                delimiter: "comma",
-                requireLast: false,
-            },
-        }],
+        rules: {
+            "unicorn/no-array-for-each": ["error"],
 
-        "@typescript-eslint/no-unused-vars": ["error"],
-        "@typescript-eslint/consistent-type-imports": ["error"],
+            "unicorn/consistent-function-scoping": ["error", {
+                checkArrowFunctions: false,
+            }],
 
-        camelcase: ["error", {
-            ignoreGlobals: true,
-            ignoreImports: true,
-        }],
+            "unicorn/no-await-expression-member": "error",
+            "unicorn/no-for-loop": "error",
+            "unicorn/no-lonely-if": "error",
+            "unicorn/no-object-as-default-parameter": "error",
+            "unicorn/prefer-array-find": "error",
+            "unicorn/prefer-array-some": "error",
+            "max-depth": ["error", 4],
 
-        "no-unneeded-ternary": ["error"],
+            "@typescript-eslint/no-misused-promises": ["error", {
+                checksSpreads: true,
+                checksVoidReturn: false,
+                checksConditionals: true,
+            }],
 
-        "no-restricted-syntax": ["error", {
-            selector: "TSEnumDeclaration",
-            message: "Don't declare enums, use literals instead",
-        }],
+            "@typescript-eslint/prefer-optional-chain": ["error"],
+            "@typescript-eslint/no-unnecessary-type-assertion": ["error"],
+            "@typescript-eslint/no-unnecessary-type-constraint": ["error"],
+            "@typescript-eslint/no-redundant-type-constituents": ["error"],
+            "@typescript-eslint/no-inferrable-types": ["error"],
 
-        "no-constant-condition": ["error"],
-        "no-constant-binary-expression": "error",
-        "no-trailing-spaces": "error",
-        "no-multi-spaces": "error",
-        "space-infix-ops": "error",
-        indent: ["error", "tab"],
+            "@typescript-eslint/no-explicit-any": ["warn", {
+                ignoreRestArgs: true,
+            }],
 
-        quotes: [2, "single", {
-            avoidEscape: true,
-        }],
+            "@stylistic/type-annotation-spacing": ["error"],
 
-        "object-curly-spacing": ["error", "always"],
-        "space-in-parens": ["error", "never"],
-        curly: [2, "all"],
-        "brace-style": ["error"],
-        "linebreak-style": ["error", "unix"],
-        semi: ["error", "never"],
-        "space-before-function-paren": ["error", "never"],
-
-        "keyword-spacing": ["error", {
-            overrides: {
-                if: {
-                    after: false,
+            "@stylistic/member-delimiter-style": ["error", {
+                multiline: {
+                    delimiter: "none",
+                    requireLast: false,
                 },
 
-                for: {
-                    after: false,
+                singleline: {
+                    delimiter: "comma",
+                    requireLast: false,
                 },
+            }],
 
-                while: {
-                    after: false,
+            "@typescript-eslint/no-unused-vars": ["error"],
+            "@typescript-eslint/consistent-type-imports": ["error"],
+
+            camelcase: ["error", {
+                ignoreGlobals: true,
+                ignoreImports: true,
+            }],
+
+            "no-unneeded-ternary": ["error"],
+
+            "no-restricted-syntax": ["error", {
+                selector: "TSEnumDeclaration",
+                message: "Don't declare enums, use literals instead",
+            }],
+
+            "no-constant-condition": ["error"],
+            "no-constant-binary-expression": "error",
+            "no-trailing-spaces": "error",
+            "no-multi-spaces": "error",
+            "space-infix-ops": "error",
+            indent: ["error", "tab"],
+
+            quotes: [2, "single", {
+                avoidEscape: true,
+            }],
+
+            "object-curly-spacing": ["error", "always"],
+            "space-in-parens": ["error", "never"],
+            curly: [2, "all"],
+            "brace-style": ["error"],
+            "linebreak-style": ["error", "unix"],
+            semi: ["error", "never"],
+            "space-before-function-paren": ["error", "never"],
+
+            "keyword-spacing": ["error", {
+                overrides: {
+                    if: {
+                        after: false,
+                    },
+
+                    for: {
+                        after: false,
+                    },
+
+                    while: {
+                        after: false,
+                    },
+
+                    catch: {
+                        after: false,
+                    },
                 },
+            }],
 
-                catch: {
-                    after: false,
-                },
-            },
-        }],
+            "padding-line-between-statements": ["error", {
+                blankLine: "always",
+                prev: "function",
+                next: "*",
+            }, {
+                blankLine: "always",
+                prev: "block-like",
+                next: "*",
+            }, {
+                blankLine: "always",
+                prev: "import",
+                next: "block-like",
+            }],
 
-        "padding-line-between-statements": ["error", {
-            blankLine: "always",
-            prev: "function",
-            next: "*",
-        }, {
-            blankLine: "always",
-            prev: "block-like",
-            next: "*",
-        }, {
-            blankLine: "always",
-            prev: "import",
-            next: "block-like",
-        }],
+            eqeqeq: "error",
+            "func-names": ["error", "never"],
 
-        eqeqeq: "error",
-        "func-names": ["error", "never"],
+            "func-style": ["error", "declaration", {
+                allowArrowFunctions: true,
+            }],
 
-        "func-style": ["error", "declaration", {
-            allowArrowFunctions: true,
-        }],
+            "prefer-const": "error",
+            "prefer-arrow-callback": "error",
+            "arrow-spacing": ["error"],
+            "implicit-arrow-linebreak": ["error", "beside"],
+            "no-multiple-empty-lines": "error",
+            "space-before-blocks": "error",
+            "comma-spacing": "error",
+            "no-unused-vars": "off",
+            "jsx-quotes": ["error", "prefer-single"],
 
-        "prefer-const": "error",
-        "prefer-arrow-callback": "error",
-        "arrow-spacing": ["error"],
-        "implicit-arrow-linebreak": ["error", "beside"],
-        "no-multiple-empty-lines": "error",
-        "space-before-blocks": "error",
-        "comma-spacing": "error",
-        "no-unused-vars": "off",
-        "jsx-quotes": ["error", "prefer-single"],
+            "simple-import-sort/imports": ["error", {
+                groups: [[
+                    "^react",
+                    "^@?\\w",
+                    "^(components|modules|utils)(/.*|$)",
+                    "^\\u0000",
+                    "^\\.\\.(?!/?$)",
+                    "^\\.\\./?$",
+                    "^\\./(?=.*/)(?!/?$)",
+                    "^\\.(?!/?$)",
+                    "^\\./?$",
+                ]],
+            }],
 
-        "simple-import-sort/imports": ["error", {
-            groups: [[
-                "^react",
-                "^@?\\w",
-                "^(components|modules|utils)(/.*|$)",
-                "^\\u0000",
-                "^\\.\\.(?!/?$)",
-                "^\\.\\./?$",
-                "^\\./(?=.*/)(?!/?$)",
-                "^\\.(?!/?$)",
-                "^\\./?$",
-            ]],
-        }],
+            "react/jsx-curly-brace-presence": ["error", {
+                props: "never",
+                children: "never",
+            }],
 
-        "react/jsx-curly-brace-presence": ["error", {
-            props: "never",
-            children: "never",
-        }],
+            "react/no-typos": "error",
+            "react/react-in-jsx-scope": "off",
+            "react/self-closing-comp": "error",
+            "react/jsx-closing-tag-location": "error",
+            "react/jsx-equals-spacing": ["error", "never"],
 
-        "react/no-typos": "error",
-        "react/react-in-jsx-scope": "off",
-        "react/self-closing-comp": "error",
-        "react/jsx-closing-tag-location": "error",
-        "react/jsx-equals-spacing": ["error", "never"],
+            "react/jsx-tag-spacing": ["error", {
+                closingSlash: "never",
+            }],
 
-        "react/jsx-tag-spacing": ["error", {
-            closingSlash: "never",
-        }],
+            "react/display-name": "off",
+            "react/prop-types": "off",
+            "react/jsx-indent": ["error", "tab"],
 
-        "react/display-name": "off",
-        "react/prop-types": "off",
-        "react/jsx-indent": ["error", "tab"],
+            "react/jsx-curly-newline": ["error", {
+                multiline: "require",
+                singleline: "forbid",
+            }],
 
-        "react/jsx-curly-newline": ["error", {
-            multiline: "require",
-            singleline: "forbid",
-        }],
+            "react/jsx-first-prop-new-line": ["error", "multiline"],
+            "react/jsx-max-props-per-line": "error",
+            "react/jsx-one-expression-per-line": "error",
 
-        "react/jsx-first-prop-new-line": ["error", "multiline"],
-        "react/jsx-max-props-per-line": "error",
-        "react/jsx-one-expression-per-line": "error",
+            "react/jsx-wrap-multilines": ["error", {
+                declaration: "parens-new-line",
+                assignment: "parens-new-line",
+                return: "parens-new-line",
+                arrow: "parens-new-line",
+                condition: "parens-new-line",
+                logical: "parens-new-line",
+                prop: "ignore",
+            }],
 
-        "react/jsx-wrap-multilines": ["error", {
-            declaration: "parens-new-line",
-            assignment: "parens-new-line",
-            return: "parens-new-line",
-            arrow: "parens-new-line",
-            condition: "parens-new-line",
-            logical: "parens-new-line",
-            prop: "ignore",
-        }],
-
-        "jest/no-disabled-tests": "error",
-        "jest/no-focused-tests": "error",
-    },
-}]);
+            "jest/no-disabled-tests": "error",
+            "jest/no-focused-tests": "error",
+        },
+    }
+];
